b20b59c32f0791a1a6f36e152a3317c1350a49ed
Steph974-Git <10466@holbertonstudents.com>
2025-04-09 15:51:56 +0200

    Refactor swap and partition functions to include array size and improve readability; add print_array calls for debugging

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,41 +6,50 @@
  * swap - Swaps two integers in an array
  * @a: Pointer to the first integer
  * @b: Pointer to the second integer
+ * @array: The array being sorted
+ * @size: Size of the array
  *
  * Return: void
  */
-void swap(int *a, int *b)
+void swap(int *a, int *b, int *array, size_t size)
 {
-	int tmp;
+	if (*a != *b)
+	{
+		int tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+		tmp = *a;
+		*a = *b;
+		*b = tmp;
+		print_array(array, size);
+	}
 }
 
 /**
  * partition - Partition an array using the Lomuto partition scheme
  * @array: The array to be partitioned
  * @low: Starting index of the partition
- * @hight: Ending index of the partition
+ * @high: Ending index of the partition
+ * @size: Size of the array
  *
  * Return: Index of the pivot element
  */
-int partition(int *array, int low, int hight)
+int partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[hight];
+	int pivot = array[high];
 	int i = low - 1;
 	int j;
 
-	for (j = low; j < hight; j++)
+	for (j = low; j < high; j++)
 	{
-		if (array[j] < pivot)
+		if (array[j] <= pivot)
 		{
 			i++;
-			swap(&array[i], &array[j]);
+			if (i != j)
+				swap(&array[i], &array[j], array, size);
 		}
 	}
-	swap(&array[i + 1], &array[hight]);
+	if (i + 1 != high)
+		swap(&array[i + 1], &array[high], array, size);
 	return (i + 1);
 }
 
@@ -55,14 +64,13 @@ int partition(int *array, int low, int hight)
  */
 void quick_sort_rec(int *array, int low, int high, size_t size)
 {
-	int pa;
+	int pivot_index;
 
 	if (low < high)
 	{
-		pa = partition(array, low, high);
-		print_array(array, size);
-		quick_sort_rec(array, low, pa - 1, size);
-		quick_sort_rec(array, pa + 1, high, size);
+		pivot_index = partition(array, low, high, size);
+		quick_sort_rec(array, low, pivot_index - 1, size);
+		quick_sort_rec(array, pivot_index + 1, high, size);
 	}
 }
 
