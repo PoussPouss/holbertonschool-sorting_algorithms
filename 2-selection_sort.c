f7ee64c867cc4b44063b3d1eecffd1d95878a3a6
PoussPouss <guillaume_font@orange.fr>
2025-04-09 15:12:54 +0200

    Optimisation de la fonction de tri par sélection en simplifiant la logique des boucles imbriquées

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,9 +25,9 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
-			{
+
 				min_index = j;
-			}
+
 		}
 
 		if (min_index != i)
@@ -35,10 +35,7 @@ void selection_sort(int *array, size_t size)
 			tmp = array[i];
 			array[i] = array[min_index];
 			array[min_index] = tmp;
-
 			print_array(array, size);
 		}
-
 	}
-
 }
